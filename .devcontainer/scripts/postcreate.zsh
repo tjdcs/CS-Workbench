80a1cfe45a86c1bbc0160e6d46040cba01203ec7
tjdcs <tucker@tjdcs.dev>
2024-02-14 20:04:35 -0800

    update submodule initiation

--- .devcontainer/scripts/postcreate.zsh
+++ .devcontainer/scripts/postcreate.zsh
@@ -1,4 +1,11 @@
 #!zsh
 
+
 cd /workspaces/DevContainer
+
+# https://www.kenmuse.com/blog/avoiding-dubious-ownership-in-dev-containers/
+# https://github.com/microsoft/vscode-remote-release/issues/7923
+git config --global --add safe.directory .
+git submodule init
+
 poetry install
--- .devcontainer/scripts/poststart.zsh
+++ .devcontainer/scripts/poststart.zsh
@@ -1,8 +1,6 @@
 #!zsh
 
-# https://www.kenmuse.com/blog/avoiding-dubious-ownership-in-dev-containers/
-# https://github.com/microsoft/vscode-remote-release/issues/7923
-git config --global --add safe.directory .
+poetry install
 
 git fetch
 git status -sb -uno
