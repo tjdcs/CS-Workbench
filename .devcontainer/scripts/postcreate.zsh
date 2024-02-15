40d76e3ee6afc3e9b10bcb73948d6f4670081af3
tjdcs <tucker@tjdcs.dev>
2024-02-14 20:33:14 -0800

    post create automatically initialize submodules and poetry install
    Take 3...

--- .devcontainer/scripts/postcreate.zsh
+++ .devcontainer/scripts/postcreate.zsh
@@ -6,6 +6,6 @@ cd /workspaces/DevContainer
 # https://www.kenmuse.com/blog/avoiding-dubious-ownership-in-dev-containers/
 # https://github.com/microsoft/vscode-remote-release/issues/7923
 git config --global --add safe.directory .
-git submodule init
+git submodule update --init
 
 poetry install
