3b2e5e9ce9ae8afae1729f499649030e76bfa4df
tjdcs <tucker@tjdcs.dev>
2024-02-16 21:31:32 -0800

    Update line ending attribute
    Introduce end-of-line normalization
    
    fix line endings
    
    fix line endings... again... i have no idea what im doing
    
    fix line endings

--- .devcontainer/scripts/post-create.zsh
+++ .devcontainer/scripts/post-create.zsh
@@ -6,7 +6,12 @@ cd /workspaces/DevContainer
 # https://www.kenmuse.com/blog/avoiding-dubious-ownership-in-dev-containers/
 # https://github.com/microsoft/vscode-remote-release/issues/7923
 git config --global --add safe.directory /workspaces/DevContainer
+git config --global --add safe.directory /workspaces/DevContainer/colour-science
+git config --global --add safe.directory /workspaces/DevContainer/colour-datasets
+git config --global --add safe.directory /workspaces/DevContainer/colour-specio
+
 git submodule update --init
 
+poetry env use 3.12
 poetry install
 poetry run python -c "import imageio;imageio.plugins.freeimage.download()"
